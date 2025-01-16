d8e9cc52d5b09912a98afb08aa895d90596e2ce9
Binaryy <ibolarinwa606@gmail.com>
2025-01-16 18:57:11 +0100

    minor corrections

--- data_ingestion/newsapi_ingestion.py
+++ data_ingestion/newsapi_ingestion.py
@@ -48,10 +48,9 @@ def fetch_news(query, page_size=10):
                 # Transform Bing News format to match existing schema
                 transformed_articles = [{
                     "title": article["name"],
-                    "description": article.get("description", ""),
-                    "snippet": article.get("snippet", ""),  # Bing News API uses description for the snippet
+                    "snippet": article["snippet"],  # Bing News API uses description for the snippet
                     "url": article["url"],
-                    "publishedAt": article.get("datePublished", ""),
+                    "publishedAt": article.get["datePublished"],
                     "source": {
                         "name": article.get("provider", [{}])[0].get("name", "Unknown")
                     }
